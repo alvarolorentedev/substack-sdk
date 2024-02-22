2f871312deaac70f658f7874061fba5db3badc27
kanekotic <me@kanekotic.com>
2024-02-22 21:04:45 +0100

    fix: export of ts

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,16 @@ import { upload } from "./image";
 import { init } from "./initialize";
 import { create } from "./note";
 import { create as createAttachment } from "./attachment";
+export default {
+    init, 
+    note: {
+        create
+    }, 
+    image: {
+        upload
+    }, 
+    attachment: {
+        create: createAttachment
+    }
+};
 Object.assign(module.exports, {init, note: {create}, image: {upload}, attachment: {create: createAttachment}});
